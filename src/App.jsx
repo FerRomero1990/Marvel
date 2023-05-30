f20cb308ee8edac862ad3120da936d6c8712dd82
FerRomero1990 <aidafernanda1990@hotmail.com>
2023-05-30 13:35:24 -0500

    set up inicial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+
+function App() {
+  
+// useEffect(() => {
+//   fetch("https://dummyjson.com/products?limit=100")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       setProducts(data.products);
+//       setFilteredProducts(data.products);
+//     });
+// }, []);
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
