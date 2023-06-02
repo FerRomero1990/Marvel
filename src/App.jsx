6a67ca0aa35d3731b827b27b742fd0a2819bbdf0
FerRomero1990 <aidafernanda1990@hotmail.com>
2023-06-02 16:00:09 -0500

    modal favorite created

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,63 @@
-// import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Comic } from './components/Comic/Comic'
-import { Layout } from './components/Layout/Layout'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Comic } from './components/Comic/Comic';
+import { Layout } from './components/Layout/Layout';
+import './App.css';
+import Nav from './components/Nav/Nav';
 
 function App() {
-
-  const [comics, setComics]=useState([])
-  const [favorites, setFavorites] = useState([])
+  const [comics, setComics] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   const addToFavorite = (addedComic) => {
-    setFavorites([...favorites, addedComic])
+    setFavorites([...favorites, addedComic]);
+  };
+
+  useEffect(() => {
+    axios
+      .get('https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=31c4e681831ac058689695f8eafa61e4&hash=398fd4b79f161f31334fab4fa30d45d0')
+      .then((res) => {
+        setComics(res.data.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const openModal = () => {
+    setModalVisible(!modalVisible);
   }
 
-  console.log(favorites)
-
-  useEffect(() =>{
-    axios.get('https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=31c4e681831ac058689695f8eafa61e4&hash=398fd4b79f161f31334fab4fa30d45d0')
-    .then(res=>{
-      setComics(res.data.data.results)
-      console.log(res.data.data.results)
-    })
-    .catch(error=>console.log(error))
-  },[])
-  
   return (
-    
+    <div>
+        <Nav/>
+        <button className='btn-favorite-comics' onClick={openModal}>Favoritos</button>
       <div>
-        <Layout />
-
-        {comics.map(com => (
-          
-            (com.description !== null && com.description !== "" && !com.thumbnail.path.includes('image_not_available') && com.description !== "#N/A") ? (
-              
-          <Comic
-            key={com.id}
-            comic={com}
-            nombreComic={com.title}
-            imagen={`${com.thumbnail.path}.${com.thumbnail.extension}`}
-            descripcion={com.description}
-            favorite={'Anadir a Favoritos'}
-            favoriteComic={() =>addToFavorite(com)}/>
-            ) : null
-          // console.log(com.description)
-          ))
-        
-            }
-            
+        {favorites.map((fav) => (
+          <Layout key={fav.id} comic={fav} favorites={fav.title} modalStatus={modalVisible} />
+        ))}
       </div>
-  )
+
+      <div>
+        {comics.map((com) =>
+          com.description !== null &&
+          com.description !== '' &&
+          !com.thumbnail.path.includes('image_not_available') &&
+          com.description !== '#N/A' ? (
+            <Comic
+              key={com.id}
+              comic={com}
+              nombreComic={com.title}
+              imagen={`${com.thumbnail.path}.${com.thumbnail.extension}`}
+              descripcion={com.description}
+              favorite={'Añadir a Favoritos'}
+              favoriteComic={() => addToFavorite(com)}
+            />
+          ) : null
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+
+function Nav() {
+
+  return(
+  
+        <div className='layout-nav'>
+          <img src='https://i0.wp.com/www.tomosygrapas.com/wp-content/uploads/2016/07/marvelstudios-7611c.jpg?resize=747%2C388' alt='Marvel Logo' className='img-logo'/>
+        </div>
+    
+  )
+}
+
+export default Nav
